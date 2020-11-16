e73480e8a62e24b3908d1fbb3da1b3549f9ce067
Danielle Koski <dkoski4@yahoo.com>
2020-11-16 13:04:35 -0800

    updated controllers

--- controllers/carbs.js
+++ controllers/carbs.js
@@ -1,9 +1,44 @@
 const Carb = require("../models/carb");
 
 module.exports = {
-    index
-}
+    index,
+    new: newCarb,
+    delete: deleteCarb,
+    update, 
+    create, 
+    edit
+};
 
 function index(req, res) {
-    res.render('index');
-}
+    Carb.find({}, function(err, carbs) {
+        res.render('carbs/index', { carbs })
+    });
+};
+
+function newCarb(req, res) {
+    res.render('carbs/new');
+};
+
+function deleteCarb(req, res) {
+    Carb.findByIdAndDelete(req.params.id, function(err, deletedCarb) {
+        res.redirect('/carbs');
+    });
+};
+
+function update(req, res) {
+    Carb.findByIdAndUpdate(req.params.id, req.body, function(err, updatedCarb) {
+        res.redirect('/carbs');
+    });
+};
+
+function create(req, res) {
+    Carb.create(req.body, function(err, carb) {
+        res.redirect('/carbs') 
+    });
+};
+
+function edit(req, res) {
+    Carb.findById(req.params.id, function(err, carb) {
+        res.render('carbs/edit', { carb });
+    });
+};
--- routes/carbs.js
+++ routes/carbs.js
@@ -6,3 +6,18 @@ const router = express.Router();
 
 // require the controller
 const carbsCtrl = require('../controllers/carbs');
+
+router.get('/', carbsCtrl.index);
+
+router.get("/new", carbsCtrl.new);
+
+router.delete("/:id", carbsCtrl.delete);
+
+router.put("/:id", carbsCtrl.update);
+
+router.post("/", carbsCtrl.create);
+
+router.get("/:id/edit", carbsCtrl.edit);
+
+
+module.exports = router;
